ee1736435e70fa767cc9dd16b90b6c75a0cd9fc5
anto__dev <antonidwm@gmail.com>
2023-05-16 22:30:54 +0100

    correction texte

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,52 +12,51 @@ import InterSectionBox from '../components/InterSectionBox'
 
 function HomePage() {
 
-  if(window.location.pathname !== '/japon'){
+  if (window.location.pathname !== '/japon') {
     document.querySelector('body').classList.remove('App--hidden')
   }
   initStatDataView();
 
- const [sizeWindow, setSizeWindow] = useState(window.innerWidth)
-  React.useEffect(()=>{
-      function windowResize(){
-        console.log('resize to : '+ window.innerHeight +'X'+window.innerWidth);
-        console.log(window.innerWidth);
-        setSizeWindow(window.innerWidth); 
-      }
-      window.addEventListener('resize', windowResize);     
-  },[])
+  const [sizeWindow, setSizeWindow] = useState(window.innerWidth)
+  React.useEffect(() => {
+    function windowResize() {
+      console.log('resize to : ' + window.innerHeight + 'X' + window.innerWidth);
+      console.log(window.innerWidth);
+      setSizeWindow(window.innerWidth);
+    }
+    window.addEventListener('resize', windowResize);
+  }, [])
 
 
   return (
     <>
-    
-    <div className='homepage'>
-      
 
-      <div className="homepage__sect1 homepage__sect1--bgImg sect">
-        <div className="sect__boxImg--big sect__boxImg">
-          <div className="sect__boxTxt ">
-            {/* Changer le titre  */}
-            <h2 className=" sect__title sect__title--2 sect__title--ecart">
-              Découvre les<br />tatouages du<br />monde
-            </h2>
-            <p className="sect__txt">
+      <div className='homepage'>
 
-              Bienvenus dans cette univers, qui est de plus en plus visible de nos jours mais qui restes, pour certains assez flou sur le fond. Car l’histoire derrière certains courant, mouvement, style sont des histoires à part entière et celui qui connait l’histoire verra ces œuvres d’un autre regard que l’inculte.
-            </p>
-            <p className="sect__txt sect__txt--ecart sect__txt--italic sect__txt--thin">
-              Alors je vous invite à voyager dans ces différents univers pour en apprendre plus sur les codes et les bases de ces styles venant des 4 coins du monde.
-            </p>
-            <BoutonNormal nameBtn={"Voir la carte"} linkBtn={'#mapWorld'} />
+
+        <div className="homepage__sect1 homepage__sect1--bgImg sect">
+          <div className="sect__boxImg--big sect__boxImg">
+            <div className="sect__boxTxt ">
+              {/* Changer le titre  */}
+              <h2 className=" sect__title sect__title--2 sect__title--ecart">
+                Découvre les<br />tatouages du<br /><i>monde</i>
+              </h2>
+              <p className="sect__txt">
+
+Bienvenus dans cet univers, qui est de plus en plus visible de nos jours mais qui restes, pour certains assez flou sur le fond. Car l’histoire derrière certains courants, mouvement, style sont des histoires à part entières et celui qui connaît l’histoire verra ces œuvres d’un autre regard que l’Inculte.              </p>
+              <p className="sect__txt sect__txt--ecart sect__txt--italic sect__txt--thin">
+                Alors je vous invite à voyager dans ses différents univers pour en apprendre plus sur les codes et les bases de ces styles venant des 4 coins du monde.
+              </p>
+              <BoutonNormal nameBtn={"Voir la carte"} linkBtn={'#mapWorld'} />
+            </div>
           </div>
         </div>
+        <InterSectionBox />
+
+
+        {sizeWindow <= 900 ? <ListMapMobile /> : <MapBox />}
+
       </div>
-      <InterSectionBox  />
-      
-
-      {sizeWindow <= 900 ? <ListMapMobile />:<MapBox/>  }
-
-    </div>
     </>
   )
 }
