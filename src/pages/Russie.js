5ec3627dc5f3d7a46fbcf988ec412fe34dd1c947
anto__dev <antonidwm@gmail.com>
2023-05-17 10:32:36 +0100

    ADD history part russia

--- src/pages/Russie.js
+++ src/pages/Russie.js
@@ -15,10 +15,13 @@ import Prisonnier3 from '../assets/img/russie/prisonnier4.jpg';
 import Prisonnier4 from '../assets/img/russie/prisonnier5.jpg';
 import ArtistPrison from '../assets/img/russie/prisonnier2.jpg'
 import CoilPrison from '../assets/img/russie/coilPrison.jpg';
+import EndArea from "../assets/img/russie/endEre.jpg";
+import CastePrisonnier from "../assets/img/russie/Caste.jpg";
+import SectionMotifHori from '../components/SectionHoriMotif'
 
 function RussiePage() {
 
-  let titleArray = ["Le collecteur", "Коллектор", "вор в законе", "Le signe de répression", "Les prisonniers et leur iconographie", "L’hygiène, une autre histoire"]
+  let titleArray = ["Le collecteur", "Коллектор", "вор в законе", <>Le signe de la&nbsp;répression</>, <>Leur&nbsp;iconographie</>, "L’hygiène, une autre histoire", "La fin d'une ère", "Les épaulettes", <>L'étoiles&nbsp;à8&nbsp;branches</>, <>La Vierge&nbsp;à l’enfant</>]
   let textArray = [
     <>A la chute de l'URSS,  on a pu voir l'iconographie de ce côté-là du monde et surtout grâce à un gardien de prison, < b > Danzig Baldaev</b>  qui a recueilli les dessins sur les prisonniers ainsi que leur signification. Grâce à lui , on sait que le tatouage se faisait peu de temps après le crime et que plus la personne en avait,  plus elle avait fait de prison et plus elle était dangereuse.
     </>,
@@ -42,17 +45,39 @@ function RussiePage() {
       <br /><br /><span className="sect__txt--thin sect__txt--italic">
         Même avec les précautions prises les maladies n'étaient pas éloignées,  certains souffraient de fièvre post tatouage,  de la syphilis,  du SIDA,  d'hépatite…
       </span>
+    </>,
+    <>
+      A la chute de l’union soviétique, l’ouverture au monde occidental, la fin de la seconde guerre mondiale et diverses crises inter-prisonniers. Le tatouage de des <b>vor v zakone</b>, devient une marque de la zone, et ne fut plus “encensée” par le reste de la population.
+      <br /><br /><span className="sect__txt--thin sect__txt--italic sect__txt--jaune"> Ils ont perdu une partie de leur statut, et de nos jours plus grand monde continue ne perpétue ce style dans cet objectif-là. Même si on continue à se faire tatouer dans les prisons, les styles ont évolué, mais les traces du passé sont et resteront présentes.</span>
+    </>,
+    <>
+      Une iconographie reprenant le style, la forme et un réalisme des épaulettes des uniformes de l’armée rouge. Avec l’ajout de grade, de symbole, elles sont portées sur les deux épaules, comme sur un costume. On en retrouve certaines avec des crânes, elles font référence aux officiers de l'allemagne du 3e reich, dont le porteur approuve l’idéologie ou souhaite provoquer une fois de plus les dirigeants soviétiques.
+      <br /><br /><span className="sect__txt--thin">
+        Elles représentent le grade des criminelle dans la hiérarchie de la mafia, dont seul les plus haut gradé les portes, normalement. Mais elles représentent aussi la défiance envers la hiérarchie militaire en spoliant l’un de leur emblème et symbole de puissance.
+      </span>
+    </>,
+    <>
+      Une étoile semblable à un design de compas, de rose des vents, avec ses 8 branches. Elle est portée par paire sur le torse et / ou sur les genoux. On trouve une autre variante, celle-ci avec un nombre de branches différent qui représente le nombre d’années passées en prison, avec une branche par année.
+      <br /><br /><span className="sect__txt--thin sect__txt--italic">
+        Son design est simple et “disponible” pour tous les détenus, à condition de respecter certains préceptes. Comme celui qui les porte ne portera jamais l'uniforme ( pour le torse) et je ne s'agenouillera devant personne et surtout pas la justice.
+      </span>
+    </>,
+    <>
+      On peut retrouver différentes compositions, mais le principe reste le même, on aura une sainte Vierge tenant un enfant dans ses bras. Les Russes étant catholiques orthodoxes, on retrouve divers motifs religieux avec une signification double.
+      <br /><br /><span className="sect__txt--thin">
+        La vierge représente la protection maternelle et divine avec la loyauté de son porteur envers Dieu et son groupe. L’enfant a une signification un peu plus sombre, car il représente une vie de crime présente depuis l’enfance et dans certains cas un emprisonnement à un jeune âge.
+      </span>
     </>
   ]
   return (
     <div className="sect page">
 
-      <SectionIntroPage BTD1={"Russie"} BTD2={"ГУЛАГ"} title={<>Direction le Goulag. честь ,боль!</>} txt={<>On se trouve dans le bloc soviétique, en voyageant de goulag à camp de travail dans la Sibérie. On part à la découverte des illustrations faites par les prisonniers sur d'autres par amour, haut-fait ou patriotisme.</>} />
-      
-      
-      <InterSectionBox WhitThext={true} textTitle={"Insalubrite & Beaute"} />
+      <SectionIntroPage BTD1={"Russie"} BTD2={"ГУЛАГ"} title={<>Direction le Goulag.<br /> честь ,боль!</>} txt={<>On se trouve dans le bloc soviétique, en voyageant de goulag à camp de travail dans la Sibérie. On part à la découverte des illustrations faites par les prisonniers sur d'autres par amour, haut-fait ou patriotisme.</>} />
+
+
+      <InterSectionBox WhitThext={true} textTitle={<>Insalubrite &&nbsp;Beaute</>} />
       {/* {la technique } */}
-      <div className="sect--height100">
+      <div className="sect--heigth100">
         <SectHoriFull BigTitle1={titleArray[0]} BigTitle12={titleArray[1]} imgArtiste1={Danzing1} textcol1={textArray[0]} textcol2={textArray[1]} textcol3={""}
           BigTitle2={titleArray[2]} imgArtiste2={VoleurLoi} textcol4={textArray[2]} textcol5={""} textcol6={""}
         />
@@ -69,8 +94,14 @@ function RussiePage() {
         <div className="hori__grid hori__box--black">
           <ContentBoxHori linkImg01={ArtistPrison} linkImg02={CoilPrison} title01={titleArray[5]} text01={textArray[5]} animImg={true} historicBox={true} colorTxt={{ color: '#FDF2DE' }} />
         </div>
+        <div className="hori__grid hori__box--black">
+          <ContentBoxHori linkImg01={EndArea} linkImg02={CastePrisonnier} title01={titleArray[6]} text01={textArray[6]} animImg={true} historicBox={true} colorTxt={{ color: '#FDF2DE' }} />
+        </div>
       </section>
       <InterSectionBox WhitThext={true} textTitle={"Les motifs amblematiques"} />
+      <div className="section sect--heigth100">
+        <SectionMotifHori />
+      </div>
 
     </div>
   )
